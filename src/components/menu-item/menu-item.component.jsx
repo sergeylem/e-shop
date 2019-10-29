9d07f26b145a66e6cea257fcd9e582e301ca9466
Sergey Lem <sergey.lem@gmail.com>
2019-10-29 21:57:32 +0600

    added slide show'

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -12,8 +12,8 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
       }}>
     </div>
     <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      <h1 className='title'>{title}</h1>
+      {/* <span className='subtitle'>SHOP NOW</span> */}
     </div>
   </div>
 );
